c2d641f90498647960c86e1a397af59c9310224c
Kenneth Ellington <kellington1@usf.edu>
2020-02-16 20:10:41 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Capstone_Arrays_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 3, 4, 5 };
+            int start = 0;
+            int end = 4;
+            int i;
+
+
+            for (i = start; myArray.Length <=end; i++)
+            {
+                Console.Write("Your number is:" + myArray[i]);
+            }
+        }
+    }
+}
